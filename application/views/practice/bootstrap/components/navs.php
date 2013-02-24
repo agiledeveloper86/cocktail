b2ad705349c8d25fd5640f6f1d95f0cbc9356535
agiledeveloper86 <friendz4ever86@gmail.com>
2013-02-24 23:34:44 +0530

    navs

--- application/views/practice/bootstrap/components/navs.php
+++ application/views/practice/bootstrap/components/navs.php
@@ -2,10 +2,113 @@
 <div class="container-fluid">
 	<div class="row-fluid">
 		<div class="span4">
+			<legend>Basic tabs</legend>
+			    <ul class="nav nav-tabs">
+			    <li class="active">
+			    <a href="#">Home</a>
+			    </li>
+			    <li><a href="#">Profiles</a></li>
+			    <li><a href="#">Messages</a></li>
+			    </ul>
+		</div>
+		<div class="span4">
+			<legend>Basic pills</legend>
+			<ul class="nav nav-pills">
+			    <li class="active">
+			    <a href="#">Home</a>
+			    </li>
+			    <li><a href="#">Profiles</a></li>
+			    <li><a href="#">Messages</a></li>
+			    </ul>
+        </div>
+        <div class="span4">
+			<legend>Disabled state</legend>
+			<ul class="nav nav-pills pull-right">
+			    <li class="active">
+			    <a href="#">Home</a>
+			    </li>
+			    <li  class="disabled" ><a href="#">Profiles</a></li>
+			    <li><a href="#">Messages</a></li>
+			    </ul>
+        </div>
+	</div>
+	<div class="row-fluid">
+		<div class="span4">
+			<legend>Stacked tabs</legend>
+			    <ul class="nav nav-tabs nav-stacked">
+			    <li class="active">
+			    <a href="#">Home</a>
+			    </li>
+			    <li><a href="#">Profiles</a></li>
+			    <li><a href="#">Messages</a></li>
+			    </ul>
+		</div>
+		<div class="span4">
+			<legend>Stacked pills</legend>
+			<ul class="nav nav-pills nav-stacked">
+			    <li class="active">
+			    <a href="#">Home</a>
+			    </li>
+			    <li><a href="#">Profiles</a></li>
+			    <li><a href="#">Messages</a></li>
+			    </ul>
+		</div>
+		<div class="span4">
+			<legend>Tabs with dropdowns</legend>
+			    <ul class="nav nav-tabs">
+			    <li class="active">
+			    <a href="#">Home</a>
+			    </li>
+			    <li><a href="#">Profiles</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle"
+					data-toggle="dropdown"
+					href="#">
+					Dropdown
+					<b class="caret"></b>
+					</a>
+					<ul class="dropdown-menu">
+					<li><a href="#">Action</a></li>
+			        <li><a href="#">Other Action</a></li>
+			        <li><a href="#">Somethin else here</a></li>
+			        <li class="divider"></li>
+			        <li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			    </ul>
+		</div>
+	</div>
+	<div class="row-fluid">
+		<div class="span4">
+			<legend>Pills with dropdowns</legend>
+			<ul class="nav nav-pills">
+			    <li class="active">
+			    <a href="#">Home</a>
+			    </li>
+			    <li><a href="#">Profiles</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle"
+					data-toggle="dropdown"
+					href="#">
+					Dropdown
+					<b class="caret"></b>
+					</a>
+					<ul class="dropdown-menu">
+					<li><a href="#">Action</a></li>
+			        <li><a href="#">Other Action</a></li>
+			        <li><a href="#">Somethin else here</a></li>
+			        <li class="divider"></li>
+			        <li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			    </ul>
+		</div>
+		<div class="span4">
 			<legend></legend>
 		</div>
 		<div class="span4">
 			<legend></legend>
 		</div>
+
 	</div>
 </div>
